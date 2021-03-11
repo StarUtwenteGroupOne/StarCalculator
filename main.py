92b609043c6f47384ee7324869d2f585db2aaf52
ivan@edmi.nl <060599IJh>
2021-03-11 12:21:18 +0100

    implemented algorithm1

--- main.py
+++ main.py
@@ -5,7 +5,10 @@
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import math
 import random
+
+from graph import Graph, Vertex, Edge
 
 
 def start():
@@ -14,7 +17,8 @@ def start():
     training_set_fault_tree = create_trainingset_fault_tree(test_bowtie)
     top_event = get_top_event(test_bowtie)
     learning_parameters = get_learning_parameters()
-    bowtie = create_quantitative_bowtie(training_set_event_tree, training_set_fault_tree, top_event, learning_parameters)
+    bowtie = create_quantitative_bowtie(training_set_event_tree, training_set_fault_tree, top_event,
+                                        learning_parameters)
     print_quantitative_bowtie(bowtie)
 
 
@@ -56,8 +60,48 @@ def get_learning_parameters():
 
 
 def create_undirected_tree(training_set):
-    print("createUndirectedTree")
-    return 1
+    weights = []
+    events_size = training_set.get_events_size()
+    for i in range(0, events_size - 1):
+        weights[i] = []
+        for j in range(1, events_size):
+            weights[i][j] = compute_mutual_information(training_set, i, j)
+
+    graph = Graph(False)
+    available_events = []
+    available_events[0] = False
+    vertices = []
+    for i in range(1, events_size):
+        available_events[i] = True
+    vertices[0] = Vertex(graph)
+    graph.add_vertex(vertices[0])
+    for step in range(1, events_size):
+        highest_i = -1
+        highest_j = -1
+        for i in range(0, events_size):
+            for j in range(1, events_size):
+                if not available_events[i] and available_events[j]:
+                    if highest_i == -1 or weights[highest_i][highest_j] <= weights[i][j]:
+                        highest_i = i
+                        highest_j = j
+        vertices[highest_j] = Vertex(graph)
+        edge = Edge(vertices[highest_i], vertices[highest_j])
+        graph.add_vertex(vertices[highest_j])
+        graph.add_edge(edge)
+    return graph
+
+
+def compute_mutual_information(training_set, event1, event2):
+    weight = 0
+    for event1_state in True, False:
+        for event2_state in True, False:
+            probability_event1 = training_set.compute_single_probability(event1, event1_state)
+            probability_event2 = training_set.compute_single_probability(event2, event2_state)
+            probability_event1_and_event2 = training_set.compute_combined_probability(event1, event1_state, event2,
+                                                                                      event2_state)
+            probability = probability_event1_and_event2 / (probability_event1 * probability_event2)
+            weight += probability_event1_and_event2 * math.log(probability, 10)
+    return weight
 
 
 def create_directed_tree(undirected_tree, top_event):
