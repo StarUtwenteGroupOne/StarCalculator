11081a57aa5291c0c5282eca3250cad4fa78b9e1
DESKTOP-56R57C7\popaa <andreypopa90@gmail.com>
2021-03-14 15:19:43 +0100

    Implemented third algorithm. Still needs testing

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 import random
+import numpy as np
+from graph import *
+from trainingset import *
 
 
 def start():
@@ -23,8 +19,8 @@ def create_quantitative_bowtie(training_set_event_tree, training_set_fault_tree,
     undirected_fault_tree = create_undirected_tree(training_set_fault_tree)
     directed_event_tree = create_directed_tree(undirected_event_tree, top_event)
     directed_fault_tree = create_directed_tree(undirected_fault_tree, top_event)
-    quantitative_event_tree = create_quantitative_event_tree(directed_event_tree, learning_parameters)
-    quantitative_fault_tree = create_quantitative_fault_tree(directed_fault_tree, learning_parameters)
+    quantitative_event_tree = create_quantitative_event_tree(directed_event_tree, training_set_event_tree)
+    quantitative_fault_tree = create_quantitative_fault_tree(directed_fault_tree, training_set_fault_tree)
     quantitative_bowtie = create_quantitative_bowtie_from_trees(quantitative_event_tree, quantitative_fault_tree)
     return quantitative_bowtie
 
@@ -65,14 +61,53 @@ def create_directed_tree(undirected_tree, top_event):
     return 1
 
 
-def create_quantitative_event_tree(directed_event_tree, learning_parameters):
+def create_quantitative_event_tree(directed_event_tree, training_set_event_tree):
+    s = []
+    G = directed_event_tree
+    tr = training_set_event_tree
+    for v in G.vertices:
+        vertex = v
+        s_i = []
+        list1 = tr.get_observations_by_event_name(v.label)
+        for e in G.edges:
+            if e.tail == vertex:
+                list2 = tr.get_observations_by_event_name(e.head.label)
+                n_i_j = 0
+                n_i = 0
+                for i in range(0, len(list1)):
+                    if list1[i] == list2[i] and list2[i]:
+                        n_i_j += 1
+                    if list1[i]:
+                        n_i += 1
+                s_i.append(n_i_j / n_i)
+        s.append(s_i)
     print("createQuantitativeEventTree")
     return 1
 
 
-def create_quantitative_fault_tree(directed_fault_tree, learning_parameters):
+def create_quantitative_fault_tree(directed_fault_tree, training_set_fault_tree):
+    cpt = []
+    G = directed_fault_tree
+    tr = training_set_fault_tree
+    for v in G.vertices:
+        cpt_i = []
+        parents = [tr.get_observations_by_event_name(v.label)]
+        helping_dict = {}
+        for e in G.edges:
+            if e.head == v:
+                parents.append(tr.get_observations_by_event_name(e.tail.label))
+        parents = np.array(parents).transpose()
+        total = parents.shape[0] + 2 ** (parents.shape[1] - 1)
+        for p in parents:
+            if helping_dict[p] is None:
+                helping_dict[p] = 1
+            else:
+                helping_dict[p] = helping_dict[p] + 1
+        for k in helping_dict.keys():
+            cpt_i.append(helping_dict[k] / total)
+        cpt.append(cpt_i)
     print("createQuantitativeFaultTree")
-    return 1
+    return cpt
 
 
 def create_quantitative_bowtie_from_trees(quantitative_event_tree, quantitative_fault_tree):
